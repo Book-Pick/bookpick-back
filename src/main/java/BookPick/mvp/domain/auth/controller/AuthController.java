aa2c728fa355bfc7f981f2a7e932c4557c09c7c2
halo <dongjin778@naver.com>
2025-10-14 20:23:32 +0900

    feat(auth) : api 버전 관리 적용

--- src/main/java/BookPick/mvp/domain/auth/controller/AuthController.java
+++ src/main/java/BookPick/mvp/domain/auth/controller/AuthController.java
@@ -1,8 +1,9 @@
 package BookPick.mvp.domain.auth.controller;
 
-import BookPick.mvp.global.ApiResponse;
+import BookPick.mvp.global.api.ApiResponse;
 import BookPick.mvp.domain.auth.dto.AuthDtos.*;
 import BookPick.mvp.domain.auth.service.AuthService;
+import BookPick.mvp.global.api.SuccessCode;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
@@ -19,10 +20,10 @@ public class AuthController {
     // 1. 회원가입
    @PostMapping("/signup")
     public ResponseEntity<ApiResponse<SignRes>> signUp(@Valid @RequestBody SignReq req) {
-        SignRes res = authService.signUp(req);
-        System.out.println(res);
+
+
         return ResponseEntity.status(HttpStatus.CREATED)   // 201
-                .body(ApiResponse.created("create",res));           // created 사용
+                .body(ApiResponse.success(SuccessCode.REGISTER_SUCCESS,authService.signUp(req)));           // code, message, DTO 반환
     }
 
     // 2. 로그인
@@ -31,9 +32,13 @@ public class AuthController {
 
         AuthRes authRes = authService.login(req, res);
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success("success", authRes))    ;      //data 에 DTO 주기
+                .body(ApiResponse.success(SuccessCode.LOGIN_SUCCESS, authRes))    ;      //data 에 DTO 주기
     }
 
 
 
 }
+
+// 제목
+// 내용
+//
