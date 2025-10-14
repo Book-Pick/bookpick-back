5d575407f72aad464433c88e8a635dec152b5076
halo <dongjin778@naver.com>
2025-10-14 23:39:36 +0900

    feat(Response Code) : 응답 코드 형식 변경

--- src/main/java/BookPick/mvp/global/api/ErrorCode.java
+++ src/main/java/BookPick/mvp/global/api/ErrorCode.java
@@ -2,26 +2,30 @@ package BookPick.mvp.global.api;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
 
-
-    // -- Auth --
-    INVALID_REQUEST(400, "INVALID_REQUEST", "잘못된 요청입니다."),   // 400
-    AUTHENTICATION_FAILED(401, "AUTHENTICATION_FAILED", "아이디 또는 비밀번호가 올바르지 않습니다."),  // 401 로그인
-    DUPLICATE_EMAIL(409, "DUPLICATE_EMAIL", "중복된 이메일 입니다."), // 409 회원가입
+    // -- Auth
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),                          // 400 회원 가입
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "중복된 이메일 입니다."),                           // 409 회원 가입
+    AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 잘 못 되었습니다."),  // 401 로그인
 
     // -- User --
+    User_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),                    //404
+
 
     // -- Post --
-    POST_NOT_FOUND(404, "POST_NOT_FOUND", "해당 게시글을 찾을 수 없습니다.");
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 큐레이션을 찾을 수 없습니다."),                    //404
 
 
-    private final int status;
-    private final String code;
+    // --  Comment --
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 댓글 찾을 수 없습니다.");                     //404
+
+
+    private final HttpStatus status;
     private final String message;
-
-
 }
+
--- src/main/java/BookPick/mvp/global/api/SuccessCode.java
+++ src/main/java/BookPick/mvp/global/api/SuccessCode.java
@@ -5,36 +5,40 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+
 @AllArgsConstructor
 @Getter
 public enum SuccessCode {
 
     // -- Auth --
-    REGISTER_SUCCESS(HttpStatus.CREATED, "REGISTER_SUCCESS", "회원가입을 성공하였습니다."),
-    LOGIN_SUCCESS(HttpStatus.OK, "LOGIN_SUCCESS", "로그인에 성공했습니다"),
+    REGISTER_SUCCESS(HttpStatus.OK, "회원가입을 성공하였습니다."),
+    LOGIN_SUCCESS(HttpStatus.OK, "로그인에 성공했습니다"),
 
     // -- User --
+    GET_USERS_SUCCESS(HttpStatus.OK, "사용자 목록 조회를 성공하였습니다."),
 
-    // -- Post --
-    POST_CREATE_SUCCESS(HttpStatus.CREATED, "POST_CREATE_SUCCESS", "게시글을 성공적으로 등록하였습니다."),
-    POST_READ_SUCCESS(HttpStatus.OK, "POST_READ_SUCCESS", "게시글 조회를 성공하였습니다."),
-    POST_UPDATE_SUCCESS(HttpStatus.OK, "POST_UPDATE_SUCCESS", "게시글을 성공적으로 수정하였습니다."),
-    POST_DELETE_SUCCESS(HttpStatus.OK, "POST_DELETE_SUCCESS", "게시글을 성공적으로 삭제하였습니다."),
-
-    // -- Pre
-    PREFERENCE_CREATED(HttpStatus.CREATED, "PREFERENCE_CREATED", "사용자의 독서 취향 정보를 성공적으로 등록하였습니다."),
-    PREFERENCE_READ_SUCCESS(HttpStatus.OK, "PREFERENCE_READ_SUCCESS", "사용자의 독서 취향 정보를 성공적으로 조회하였습니다."),
-    PREFERENCE_UPDATED(HttpStatus.OK, "PREFERENCE_UPDATED", "사용자의 독서 취향 정보를 성공적으로 수정하였습니다."),
-    PREFERENCE_DELETED(HttpStatus.OK, "PREFERENCE_DELETED", "사용자의 독서 취향 정보를 성공적으로 삭제하였습니다.");
+    // -- Quration --
+    CURATION_CREATE_SUCCESS(HttpStatus.CREATED, "큐레이션을 성공적으로 등록하였습니다."),
+    CURATION_LIST_READ_SUCCESS(HttpStatus.OK, "큐레이션 목록을 성공적으로 조회하였습니다."),
+    CURATION_DETAIL_READ_SUCCESS(HttpStatus.OK, "큐레이션을 성공하였습니다."),
+    CURATION_UPDATE_SUCCESS(HttpStatus.OK, "큐레이션을 성공적으로 수정하였습니다."),
+    CURATION_DELETE_SUCCESS(HttpStatus.OK, "큐레이션을 성공적으로 삭제하였습니다."),
 
 
+    // -- Comment --
+    COMMENT_CREATE_SUCCESS(HttpStatus.CREATED, "댓글을 성공적으로 등록하였습니다."),
+    COMMENT_LIST_READ_SUCCESS(HttpStatus.OK, "댓글 목록을 성공적으로 조회하였습니다."),
+    COMMENT_LIST_EMPTY(HttpStatus.OK, "댓글이 없습니다."),
+    COMMENT_READ_SUCCESS(HttpStatus.OK, "댓글을 성공적으로 조회하였습니다."),
+    COMMENT_UPDATE_SUCCESS(HttpStatus.OK, "댓글을 성공적으로 수정하였습니다."),
+    COMMENT_DELETE_SUCCESS(HttpStatus.OK, "댓글을 성공적으로 삭제하였습니다.");
 
 
-    private final HttpStatus status;    // 상태 코드 번호
-    private final String code;          // 상태 설명 (개발용)
+    private final HttpStatus status;
     private final String message;       // 상태 설명 (사용자 친화적)
-
-
-
-
 }
+
+
+
+
+
