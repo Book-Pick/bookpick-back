8eecdfcfa354e9710b7af392baebe83fe61fda51
halo <dongjin778@naver.com>
2025-10-16 21:03:42 +0900

    chore : 오타수정

--- src/main/java/BookPick/mvp/global/api/ApiResponse.java
+++ src/main/java/BookPick/mvp/global/api/ApiResponse.java
@@ -19,8 +19,8 @@ public class ApiResponse<T> {
 
 
     // -- Success --
-    public static <T> ApiResponse<T> success(SuccessCode successMessage, T data) {        // <T>를 반환 타입 앞에 써주는 이유 : 컴파일시 해당 메서드의 반환 타입을 알기 위해서, 런타임시 파라미터로 들어온 T를 static 상황(컴파일 상황)에서는 모르기 때문이다. 컴퓨일이 런타임 이전에 일어나기 때문에
-        return new ApiResponse<T>(successMessage.getStatus().value(), successMessage.getMessage(), data);
+    public static <T> ApiResponse<T> success(SuccessCode successCode, T data) {        // <T>를 반환 타입 앞에 써주는 이유 : 컴파일시 해당 메서드의 반환 타입을 알기 위해서, 런타임시 파라미터로 들어온 T를 static 상황(컴파일 상황)에서는 모르기 때문이다. 컴퓨일이 런타임 이전에 일어나기 때문에
+        return new ApiResponse<T>(successCode.getStatus().value(), successCode.getMessage(), data);
     }
 
     // -- Error --
