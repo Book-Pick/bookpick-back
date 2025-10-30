030e86978e07eda48ca557169c4aebfad18cf4d1
halo <dongjin778@naver.com>
2025-10-30 19:40:41 +0900

    feat : 각 큐레이션 리스트 조회 기능 모듈화 (with 프로바이더)

---
+++ src/main/java/BookPick/mvp/domain/curation/service/provider/CurationPopularProvider.java
@@ -0,0 +1,22 @@
+package BookPick.mvp.domain.curation.service.provider;
+
+
+import BookPick.mvp.domain.curation.entity.Curation;
+import BookPick.mvp.domain.curation.repository.CurationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CurationPopularProvider {
+
+    private final CurationRepository curationRepository;
+
+    public List<Curation> fetch(Long cursor, Pageable pageable) {
+        return curationRepository.findCurationsByPopularity(cursor, pageable);
+    }
+}
+
---
+++ src/main/java/BookPick/mvp/domain/curation/service/provider/CurationSimilarityProvider.java
@@ -0,0 +1,22 @@
+package BookPick.mvp.domain.curation.service.provider;
+
+
+import BookPick.mvp.domain.curation.entity.Curation;
+import BookPick.mvp.domain.curation.repository.CurationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CurationSimilarityProvider {
+
+    private final CurationRepository curationRepository;
+
+    public List<Curation> fetch(Long cursor, Pageable pageable) {
+        // TODO: 유사도 알고리즘 구현 후 변경 예정
+        return curationRepository.findCurations(cursor, pageable);
+    }
+}
