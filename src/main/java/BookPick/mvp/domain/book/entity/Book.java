78e3ab00a78c115540a8aad5232dbe82b785e5d9
halo <dongjin778@naver.com>
2025-11-11 23:43:54 +0900

    feat : new 생성자 지양, 도서 클래스 펙토리 메서드 구현

--- src/main/java/BookPick/mvp/domain/book/entity/Book.java
+++ src/main/java/BookPick/mvp/domain/book/entity/Book.java
@@ -1,6 +1,7 @@
 package BookPick.mvp.domain.book.entity;
 
 import BookPick.mvp.domain.author.entity.Author;
+import BookPick.mvp.domain.book.dto.preference.BookDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,6 +9,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -38,5 +40,17 @@ public class Book {
 
     public Book() {
 
+    }
+
+    public static Book from(BookDto bookDto) {
+
+        List<String> authors
+
+        return Book.builder()
+                .title(bookDto.title())
+                .authors(new HashSet<>(bookDto.authors()))
+                .image(bookDto.image())
+                .isbn(bookDto.isbn())
+                .build();
     }
 }
