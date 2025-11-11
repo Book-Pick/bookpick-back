beea63193932153db1b2c1f1a8c0902da5502030
halo <dongjin778@naver.com>
2025-11-11 23:44:31 +0900

    feat : 에러 코드 관심사 도메인 별 분리를 위한 책 찾을 수 없을 때 예외처리 구현

---
+++ src/main/java/BookPick/mvp/domain/author/enums/AuthroErrorCode.java
@@ -0,0 +1,19 @@
+package BookPick.mvp.domain.author.enums;
+
+import BookPick.mvp.global.api.ErrorCode.ErrorCodeInterface;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum AuthroErrorCode implements ErrorCodeInterface {
+
+
+    BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 책을 찾을 수 없습니다.");
+
+
+
+    private final HttpStatus status;
+    private final String message;
+}
