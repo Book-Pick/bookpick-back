5b2918753b361a4129122c6b78bae0372166bf43
halo <dongjin778@naver.com>
2025-12-29 23:51:23 +0900

    feat[https://github.com/Book-Pick/bookpick-front/issues/57] : 댓글 최신 불러오기 추가

--- src/main/java/BookPick/mvp/domain/comment/service/PagenationService.java
+++ src/main/java/BookPick/mvp/domain/comment/service/PagenationService.java
@@ -1,4 +1,4 @@
-package BookPick.mvp.domain.comment.controller;
+package BookPick.mvp.domain.comment.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
