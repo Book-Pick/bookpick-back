bda86afad28d0822b280def57ae852fc6b052568
halo <dongjin778@naver.com>
2026-01-26 23:34:34 +0900

    feat: 삭제된 댓글 복구 가능하도록 soft delete 적용

--- src/main/java/BookPick/mvp/domain/comment/repository/CommentRepository.java
+++ src/main/java/BookPick/mvp/domain/comment/repository/CommentRepository.java
@@ -13,13 +13,13 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.parent LEFT JOIN FETCH c.user WHERE c.curation.id = :curationId")
+    @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.parent LEFT JOIN FETCH c.user WHERE c.curation.id = :curationId AND c.deletedAt IS NULL")
     Page<Comment> findByCurationId(@Param("curationId") Long curationId, Pageable pageable);
 
 
     @Query("""
                 SELECT cm FROM Comment cm
-                WHERE cm.curation.user.id = :userId
+                WHERE cm.curation.user.id = :userId AND cm.deletedAt IS NULL
                 ORDER BY cm.createdAt DESC
             """)
     List<Comment> findLatestCommentsByUserId(
