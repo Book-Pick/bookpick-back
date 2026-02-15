a13f0c26acdce1b811b3b851eef6e720799abf11
halo <dongjin778@naver.com>
2026-02-15 22:37:51 +0900

    feat[#104]: 임시코드 만료 정리를 위해, @EnableScheduling 추가
    - OAuthTempCodeStore의 @Scheduled 동작을 위한 설정

--- src/main/java/BookPick/mvp/BookPickApplication.java
+++ src/main/java/BookPick/mvp/BookPickApplication.java
@@ -3,9 +3,11 @@ package BookPick.mvp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableJpaAuditing // ← 이거 있어?
+@EnableJpaAuditing
+@EnableScheduling
 public class BookPickApplication {
 
     public static void main(String[] args) {
